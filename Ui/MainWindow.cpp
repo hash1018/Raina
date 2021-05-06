6f8aa85d4bcfc776b664a4789a41ea61d212c56e
hash1018 <46293206+hash1018@users.noreply.github.com>
2021-05-06 12:09:29 +0900

    1234

--- Ui/MainWindow.cpp
+++ Ui/MainWindow.cpp
@@ -1,15 +1,17 @@
+
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
+
+
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+
     delete ui;
 }
 
--- Ui/MainWindow.h
+++ Ui/MainWindow.h
@@ -1,3 +1,4 @@
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -7,15 +8,18 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
+    
     Q_OBJECT
+
+private:
+    Ui::MainWindow* ui;
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
 };
+
+
 #endif // MAINWINDOW_H
