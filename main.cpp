76d6bd8371d01d38318bc889b8749559420c9cbe
hash1018 <46293206+hash1018@users.noreply.github.com>
2021-05-06 14:48:13 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,18 @@
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
 #include <stdlib.h>
-#define DEBUG_NEW new(_NORMAL_BLOCK, THIS_FILE, __LINE__ )
+
+/*
+#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__ )
+*/
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _CLIENT_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
 
 #endif
 
