dc2267ee7f9f7302d3e1f2f5651392980535b1ed
SeunghoHa <sh.ha1018@gmail.com>
2021-05-06 18:08:35 +0900

    d

--- main.cpp
+++ main.cpp
@@ -27,10 +27,12 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     
+
+#ifdef Q_OS_WIN
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     //_CrtSetBreakAlloc(8360);
     int* temp = new int;
-
+#endif
    
     
 
